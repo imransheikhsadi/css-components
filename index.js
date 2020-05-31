9ecb8af0e96a547430ddc461dbb056849d9669cf
Imran-S-heikh <imransheikhsadi@gmail.com>
2020-05-31 07:12:20 +0600

    Top Bar Done

--- index.js
+++ index.js
@@ -1,14 +1,21 @@
 const webfontsGenerator = require('webfonts-generator');
 const chokidar = require('chokidar');
 const Atomizer = require('atomizer');
-const write = require('write');
+const config   = require('./atomic.css.config');
 const http = require('http').createServer(requestListener);
+const open = require('open');
 const io = require('socket.io')(http);
 const fs = require('fs');
 
 const host = 'localhost';
 const port = 8000;
-http.listen(port, host)
+http.listen(port, host,()=> {
+    console.log('Server started at http://'+host+':'+port);
+    open('http://'+host+':'+port);
+});
+
+const watchPath = '.'
+var atomizer = new Atomizer({ verbose: true });
 
 let filePath;
 let basePath;
@@ -44,7 +51,7 @@ function requestListener(req, res) {
 
             webfontsGenerator({
                 files: filesArr,
-                writeFiles: true,
+                writeFiles: false,
                 dest: basePath+'assets'
             }, (err, result) => {
                 iconFile = result;
@@ -52,50 +59,41 @@ function requestListener(req, res) {
                 res.end(result.generateCss());
             })
         })
-        // console.log(files);
-
-        // fs.readFile(basePath+'assets/iconfont.css','utf8',(err,data)=>{
-        //     res.writeHead(200);
-        //     res.end(data);
-        // })
-
 
     }else if(req.url.match('woff2')){
-        // res.writeHead(200,{ "Content-Type": "text/css" });
+        res.writeHead(200,{ "Content-Type": "font/woff2" });
         res.end(iconFile.woff2,'binary');
+    }else if(req.url.match('woff')){
+        res.writeHead(200,{ "Content-Type": "font/woff" });
+        res.end(iconFile.woff,'binary');
+    }else if(req.url.match(/\.(jpeg|png|jpg)$/)){
+        fs.readFile(basePath.replace(/.$/,'')+req.url,(err,data)=>{
+            if (data) {
+                res.writeHead(200, {'Content-Type': 'image/jpeg'});
+                res.end(data)
+            }
+        })
     }
-    console.log(req.url);
+    
 };
 
-
-
-
-
-
-
-
-
-
-
-
-
 // Atomizer for css and out put html
-const config = {
-    "breakPoints": {
-        'xm': '@media(min-width:0)',
-        'sm': '@media(min-width:576px)',
-        'md': '@media(min-width:768px)',
-        'lg': '@media(min-width:992px)',
-        'xl': '@media(min-width:1200px)',
-    }
+// const config = {
+//     "breakPoints": {
+//         'xm': '@media(min-width:0)',
+//         'sm': '@media(min-width:576px)',
+//         'md': '@media(min-width:768px)',
+//         'lg': '@media(min-width:992px)',
+//         'xl': '@media(min-width:1200px)',
+//         'Msm': '@media(max-width:576px)',
+//         'Mmd': '@media(max-width:768px)',
+//         'Mlg': '@media(max-width:992px)',
+//         'Mxl': '@media(max-width:1200px)',
+//     }
 
-}
+// }
 
 
-
-
-const watchPath = '.'
-var atomizer = new Atomizer({ verbose: true });
 
 const watcher = chokidar.watch(watchPath, {
     ignored: ['node_modules'],
@@ -111,27 +109,45 @@ const handleEvent = (path) => {
 
     fs.readFile(path, 'utf8', (err, data) => {
         if (data && dest) {
+
+            let helper_class = data.match(/\$[A-Za-z]+\(([a-z0-9\,\[\]\s\:\'\"]+\)|\))/g);
+            with(config.helpers){
+                if (helper_class) {
+                    helper_class.forEach(element => {
+                        data = data.replace(element, eval(element).join(' '));
+                    });
+                }
+            }
+
             // Parse text to find Atomic CSS classes
             let found_class = atomizer.findClassNames(data);
             // Generate Atomizer configuration from an array of Atomic classnames
-            var finalConfig = atomizer.getConfig(found_class, config);
+            var finalConfig = atomizer.getConfig(found_class, config.configs);
             // Generate Atomic CSS from configuration
+
+
             html = data.replace('</body>', `
             <script src="socket.js"></script>
             <script>
-            const socket = io('http://${host}:${port}');
+            let socket = io('http://${host}:${port}');
             socket.on('save',(data)=>{
                 location.reload();
             })
             </script>
             </body>`)
+
+
             css = atomizer.getCss(finalConfig);
             io.emit('save', 'File changed');
         }
     });
 }
 
-watcher.on('change', handleEvent);
+watcher.on('change', (path) => {
+    if (path.includes('index.html')) {
+        handleEvent(path)
+    }
+});
 watcher.on('add', (path) => {
     if (path.includes('index.html')) {
         handleEvent(path)
